435edefdfbd2e27d8746b661af81a60153786e7c
Frank Chao <aaefiikmnnnr@gmail.com>
2017-09-01 23:18:44 +0800

    Fix "Building to Assets folder is not allowed"

--- Assets/Scripts/Editor/Builder.cs
+++ Assets/Scripts/Editor/Builder.cs
@@ -6,7 +6,7 @@ public class Builder {
 	public static void BuildProjectAndroid()
 	{
 		BuildPipeline.BuildPlayer(UnityEditor.EditorBuildSettings.scenes, 
-			System.IO.Path.Combine(Application.dataPath, "artifacts"), 
+			System.IO.Path.Combine(Application.dataPath, "../artifacts"), 
 			BuildTarget.Android, BuildOptions.None);
 	}
 }
