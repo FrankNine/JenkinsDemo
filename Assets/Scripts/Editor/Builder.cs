682519ed458ff84a0e67a9da521fbf73db2efbf6
Frank Chao <aaefiikmnnnr@gmail.com>
2017-09-01 23:06:12 +0800

    Initial commit Builder script

---
+++ Assets/Scripts/Editor/Builder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Builder {
+
+	public static void BuildProjectAndroid()
+	{
+		BuildPipeline.BuildPlayer(UnityEditor.EditorBuildSettings.scenes, 
+			System.IO.Path.Combine(Application.dataPath, "artifacts"), 
+			BuildTarget.Android, BuildOptions.None);
+	}
+}
